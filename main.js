aa5d14457e6d321a367f3340f2e7964f931ebd9a
Aleksei <kilroygood@gmail.com>
2020-04-10 21:03:14 +0300

    x

--- main.js
+++ main.js
@@ -21,66 +21,61 @@ document.addEventListener("DOMContentLoaded", function (event) {
     dragElem.onclick = ff();
     dragElem2.onclick = ff2();
     dragElem3.onclick = ff3();
-    
+
+    var remD = document.getElementById('btn')
+    remD.addEventListener('click', remove())
+      
+    function remove(){
+      var item = document.getElementById('btn');
+        var parent = document.getElementById('btnDiv');
+      parent.removeChild(item);
+      console.log('gg2')
+    }
+
 
     function move(a) {
-        a.onmousedown = function(e) {
-    
-            var coords = getCoords(a);
-            var shiftX = e.pageX - coords.left;
-            var shiftY = e.pageY - coords.top;
-          
-            a.style.position = 'absolute';
-            document.body.appendChild(a);
-            moveAt(e);
-          
-            a.style.zIndex = 1000; 
-          
-            function moveAt(e) {
-                a.style.left = e.pageX - shiftX + 'px';
-                a.style.top = e.pageY - shiftY + 'px';
-            }
-          
-            document.onmousemove = function(e) {
-              moveAt(e);
-            };
-          
-            a.onmouseup = function() {
-              document.onmousemove = null;
-              a.onmouseup = null;
-            };
-          
-          }
-          
-          a.ondragstart = function() {
-            return false;
-          };
-          
-          function getCoords(elem) {
-            var box = elem.getBoundingClientRect();
-            return {
-              top: box.top + pageYOffset,
-              left: box.left + pageXOffset
-            };
-          }
-    }
-    
+    a.onmousedown = function(e) {
 
-    // Element.prototype.makeDraggable=function(){
-    //     var o=this
-    //     o.onmousedown=function(e){
-    //       var offsetX=e.pageX-parseInt(o.style.left)
-    //       var offsetY=e.pageY-parseInt(o.style.top)
-    //       document.onmousemove=function(e) {
-    //         o.style.left=Math.max(Math.min(e.pageX-offsetX,o.parentNode.clientWidth-o.clientWidth),0)+'px'
-    //         o.style.top=Math.max(Math.min(e.pageY-offsetY,o.parentNode.clientHeight-o.clientHeight),0)+'px'
-    //       }
-    //       document.onmouseup = function(e) {
-    //         document.onmousemove=o.onmouseup=null
-    //       }
-    //     }
-    //     o.ondragstart = function(){return 0}
-    //   }
-    //   document.getElementById('object1').makeDraggable()
+      var x = document.getElementById('btn')
+      var coords = getCoords(a);
+      var shiftX = e.pageX - coords.left;
+      var shiftY = e.pageY - coords.top;
     
+      x.classList.add('active')
+      a.style.position = 'absolute';
+      document.body.appendChild(a);
+      moveAt(e);
+    
+      a.style.zIndex = 1000; 
+    
+      function moveAt(e) {
+          a.style.left = e.pageX - shiftX + 'px';
+          a.style.top = e.pageY - shiftY + 'px';
+      }
+    
+      document.onmousemove = function(e) {
+        moveAt(e);
+      };
+    
+      a.onmouseup = function() {
+        document.onmousemove = null;
+        a.onmouseup = null;
+      };
+      
+      }
+      
+      a.ondragstart = function() {
+        return false;
+      };
+      
+      function getCoords(elem) {
+        var box = elem.getBoundingClientRect();
+        return {
+          top: box.top + pageYOffset,
+          left: box.left + pageXOffset
+        };
+      }
+    }
+
+
 });
